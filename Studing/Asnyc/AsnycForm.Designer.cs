4820c7190cc37c81a099a3096a34993e27d1c76b
gulinqian <549276900@qq.com>
2016-11-27 23:33:10 +0800

    关于异步

---
+++ Studing/Asnyc/AsnycForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Asnyc
+{
+    partial class AsnycForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAsnyc = new System.Windows.Forms.Button();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsnycAdvance = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAsnyc
+            // 
+            this.btnAsnyc.Location = new System.Drawing.Point(61, 51);
+            this.btnAsnyc.Name = "btnAsnyc";
+            this.btnAsnyc.Size = new System.Drawing.Size(75, 23);
+            this.btnAsnyc.TabIndex = 0;
+            this.btnAsnyc.Text = "异步调用";
+            this.btnAsnyc.UseVisualStyleBackColor = true;
+            this.btnAsnyc.Click += new System.EventHandler(this.btnAsnyc_Click);
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(61, 115);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 1;
+            this.btnSync.Text = "同步调用";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // btnAsnycAdvance
+            // 
+            this.btnAsnycAdvance.Location = new System.Drawing.Point(61, 171);
+            this.btnAsnycAdvance.Name = "btnAsnycAdvance";
+            this.btnAsnycAdvance.Size = new System.Drawing.Size(75, 23);
+            this.btnAsnycAdvance.TabIndex = 2;
+            this.btnAsnycAdvance.Text = "异步进阶";
+            this.btnAsnycAdvance.UseVisualStyleBackColor = true;
+            this.btnAsnycAdvance.Click += new System.EventHandler(this.btnAsnycAdvance_Click);
+            // 
+            // AsnycForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnAsnycAdvance);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.btnAsnyc);
+            this.Name = "AsnycForm";
+            this.Text = "AsnycForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAsnyc;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsnycAdvance;
+    }
+}
+
---
+++ Studing/Asnyc/AsnycForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asnyc
+{
+    public partial class AsnycForm : Form
+    {
+        public AsnycForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 所有的异步都是基于委托实现的 
+        /// </summary>
+        /// <param name="str"></param>
+        private delegate void DoSomething(string str);
+        private void btnAsnyc_Click(object sender, EventArgs e)
+        {
+            //实例化一个委托
+            //DoSomething dosomething = new DoSomething(t => Console.WriteLine("这里是异步调用，str是{0}",t));
+            //DoSomething dosomething =t => Console.WriteLine("这里是btnAsnyc_Click，str是{0},当前线程Id:{1}", t,Thread.CurrentThread.ManagedThreadId);
+            //dosomething("直接调用");//主线程调用
+            //dosomething.Invoke("Invoke调用");//主线程调用
+            //dosomething.BeginInvoke("BeginInvoke调用", null, null);//这就是异步调用  子线程在调用
+
+            Console.WriteLine("**************btnAsnyc_Click异步开始******************");
+            DoSomething todo = DoSomethingTest;
+            for (int i = 0; i < 5; i++)
+            {
+                todo.BeginInvoke(string.Format("btnAsnyc_Click异步"),null,null);
+            }
+            Console.WriteLine("**************btnAsnyc_Click异步结束******************");
+
+            /***************总结异步的特点*******************
+            1.同步会卡住界面，异步不会；原因：异步启动了子线程，主线程得到释放；
+            2.同步方法会慢，异步方法会快，但是消耗的资源比较多，因为启动了多个子线程；
+            3.异步线程启动是无序的，结束也是无序的，由操作系统决定；
+            
+            *****************总结异步的特点***********/
+
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("**************btnSync_Click同步开始******************");
+            for (int i = 0; i < 5; i++)
+            {
+                DoSomethingTest("btnSync_Click同步");
+            }
+            Console.WriteLine("**************btnSync_Click同步结束******************");
+        }
+
+        private void DoSomethingTest(string name)
+        {
+            long lResult = 0;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                lResult += i;
+            }
+            Thread.Sleep(1000);
+            watch.Stop();
+            Console.WriteLine("这里是{0}，计算结果是{1}，耗时{2},,当前线程Id{3}", name, lResult, watch.ElapsedMilliseconds, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void btnAsnycAdvance_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("**************btnAsnycAdvance_Click异步进阶开始******************");
+            DoSomething todo = DoSomethingTest;
+            AsyncCallback asyncCallback = t=> Console.WriteLine("这是asyncCallback,当前线程Id:{0}，AsyncState={1}", Thread.CurrentThread.ManagedThreadId,t.AsyncState);
+            //todo.BeginInvoke 的执行结果就是IAsyncResult   其实t就是异步执行的结果
+            IAsyncResult result= todo.BeginInvoke(string.Format("btnAsnycAdvance_Click异步进阶"), asyncCallback, "我是什么啊");
+
+
+            //异步主线程等待三种方式
+
+            //1 
+            //result.AsyncWaitHandle.WaitOne(-1);//无限期等待
+
+
+            //2
+            //while (!result.IsCompleted)
+            //{
+            //    Console.WriteLine("请继续等待.....");
+            //}
+
+
+            //3
+            todo.EndInvoke(result);
+
+
+
+            Console.WriteLine("**************btnAsnycAdvance_Click异步进阶结束******************");
+        }
+    }
+}
