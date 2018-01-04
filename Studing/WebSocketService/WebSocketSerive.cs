d5015a79c61c47b4af790b235771416523eddee8
顾林仟 <549276900@qq.com>
2018-01-04 18:24:29 +0800

    添加websocket

---
+++ Studing/WebSocketService/WebSocketSerive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebSocketService
+{
+    public class WebSocketSerive
+    {
+      public WebSocket webSocket { get; set; }
+
+      
+    }
+}
--- Studing/helloworld/Program.cs
+++ Studing/helloworld/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using myLibrary;
 namespace helloworld
 {
@@ -13,22 +15,60 @@ namespace helloworld
             //Console.WriteLine("press any key to continue!");
             //Console.ReadKey(true);
 
-            int rows = Console.WindowHeight;
-            Console.Clear();
-            do
+            //int rows = Console.WindowHeight;
+            //Console.Clear();
+            //do
+            //{
+            //    if(Console.CursorTop>=rows||Console.CursorTop==0)
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("按 Enter键退出，或者输入一些字符+Enter键继续；");
+            //    }
+            //    string inputstr = Console.ReadLine();
+            //    if(string.IsNullOrWhiteSpace(inputstr))
+            //    {
+
+            //        break;
+            //    }
+            //    Console.WriteLine($"Input: {inputstr} {"Begins with uppercase? ",30}: {(inputstr.StartsWithUpper() ? "Yes" : "No")}\n");
+            //} while (true);
+
+            //Dictionary<string, string> dic = new Dictionary<string, string>();
+            //dic.Add("10001", "2");
+            //dic.Add("10002", "4");
+            //dic.Add("10003", "6");
+
+            //List<string> keys = new List<string>(dic.Keys);
+
+            //for (int i = 0; i < dic.Count; i++)
+            //{
+
+            //    Console.WriteLine($"key:{keys[i]},value:{dic[keys[i]]}");
+            //    if (i == 2)
+            //    {
+            //        dic.Remove(keys[i]);
+            //    }
+            //}
+            //Console.WriteLine("*******************************");
+            //for (int i = 0; i < dic.Count; i++)
+            //{
+            //    Console.WriteLine($"key:{keys[i]},value:{dic[keys[i]]}");
+            //}
+
+
+            StringBuilder paramList = new StringBuilder();
+            var relateIdList = new List<string> {"a","b","c" };
+            foreach (var item in relateIdList)
             {
-                if (Console.CursorTop >= rows || Console.CursorTop == 0)
+                if (string.IsNullOrEmpty(item))
                 {
-                    Console.Clear();
-                    Console.WriteLine("按 Enter键退出，或者输入一些字符+Enter键继续；");
+                    continue;
                 }
-                string inputstr = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(inputstr))
-                {
-                    break;
-                }
-                Console.WriteLine($"Input: {inputstr} {"Begins with uppercase? ",30}: {(inputstr.StartsWithUpper() ? "Yes" : "No")}\n");
-            } while (true);
+                paramList.AppendFormat("'{0}',", item);
+            }
+            Console.WriteLine(paramList.ToString().TrimEnd(','));
+
+            Console.Read();
 
         }
     }
