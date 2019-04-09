5aa836593bb2ddfb65bbfa8424dc1c5f1c2480bb
glq13551 <glq13551@ly.com>
2019-04-09 17:42:48 +0800

    图片格式判定和转换

---
+++ Studing/CommonLq/ImageUnity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace CommonLq
+{
+    public static class ImageUnity
+    {
+        /// <summary>
+        /// 转换图片格式
+        /// </summary>
+        /// <param name="originStream"></param>
+        /// <param name="imageFormat"></param>
+        /// <returns></returns>
+        public static Stream ConvertImageFormat(this Stream originStream, ImageFormat imageFormat)
+        {
+            Image image = Image.FromStream(originStream);
+            var newstram = new MemoryStream();
+            image.Save(newstram, imageFormat);
+            newstram.Position = 0;
+            return newstram;
+        }
+        /// <summary>
+        /// 判断此图片格式是想要的图片格式
+        /// </summary>
+        /// <param name="originStream"></param>
+        /// <param name="needformat"></param>
+        /// <returns></returns>
+        public static bool IsSameImageFormat(this Stream originStream, ImageFormat needformat)
+        {
+            Image image = Image.FromStream(originStream);
+            return needformat.Equals(image.RawFormat);
+        }
+    }
+}
