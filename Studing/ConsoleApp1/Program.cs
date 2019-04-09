c7a7ae25a9264ca70c5d6c69f14cb75cfdf2385a
glq13551 <glq13551@ly.com>
2019-04-09 17:24:21 +0800

    vue

---
+++ Studing/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+            for (int a = 1; a < 100; a++)
+            {
+                //tasks.Add(Task.Run(() => { Wash_SupplierId_s($" dppid>{a * 10000} and dppid<={(a + 1) * 10000} "); }));
+                int b = a;
+                tasks.Add(Task.Run(() => { hhhh($" dppid>{b} and dppid<={b + 1} "); }));
+                if (tasks.Count > 10)
+                {
+                    tasks.RemoveAt(Task.WaitAny(tasks.ToArray()));
+                }
+            }
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+        private static void hhhh(string s)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine(s+"....."); 
+        }
+    }
+}
